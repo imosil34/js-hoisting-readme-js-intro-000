fe31bba3983c6bd738fec95e4dd568744eb57fe6
IMOH SILAS <imosil34@gmail.com>
2020-05-31 18:37:28 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -15,10 +15,10 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  var thisIsCrazy;
+  
   thisIsCrazy();
 
-  thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
